15779b649a8ba0bd43941dedeae05466b47938e2
jhon65496 <jhon65496@yandex.ru>
2023-10-31 20:56:48 +0300

    ch

--- GitBranchMergeTest03/Class7.cs
+++ GitBranchMergeTest03/Class7.cs
@@ -7,19 +7,20 @@ using System.Threading.Tasks;
 namespace GitBranchMergeTest03
 {
     internal class Class7
-    {   
+    {
         public Class7()
         {
-                    
+
         }
 
-        // br-2 | modif-1     
+        // br-1 | modif-2     
         public void Method1()
         {
 
         }
-        
-        
+
+
+        // br-2 | modif-2     
         public void Method2()
         {
 
@@ -29,5 +30,12 @@ namespace GitBranchMergeTest03
         {
 
         }
+
+        // br-2 | modif-1     
+        public void Method4()
+        {
+
+        }
+
     }
 }
